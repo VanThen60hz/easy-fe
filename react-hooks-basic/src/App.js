6a4b22bc8d6b034bce2abb43ccf83bcf79bf8d1f
VanThen60hz <nguyenvthang2409@gmail.com>
2023-12-21 01:13:14 +0700

    add code use state lab 3

--- react-hooks-basic/src/App.js
+++ react-hooks-basic/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.scss";
 import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -18,10 +19,23 @@ function App() {
     setTodoList(newTodoList);
   };
 
+  const handleTodoFormSubmit = (formValues) => {
+    const newTodo = {
+      id: todoList[todoList.length - 1].id + 1,
+      ...formValues,
+    };
+
+    console.log(newTodo);
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  };
+
   return (
     <div className="app">
       <h1>React hooks - TodoList</h1>
 
+      <TodoForm onSubmit={handleTodoFormSubmit} />
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
