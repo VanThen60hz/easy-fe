02d3f1e1506bc516a8adb4910093ba1e81932113
VanThen60hz <nguyenvthang2409@gmail.com>
2024-01-18 02:27:55 +0700

    check alias

--- react-hooks-basic/src/App.js
+++ react-hooks-basic/src/App.js
@@ -12,111 +12,112 @@ import MagicBox from "./components/MagicBox/index";
 import Hero from "./components/Hero/Hero";
 
 function App() {
-  // const [todoList, setTodoList] = useState([
-  //   { id: 1, title: "Toothbrush" },
-  //   { id: 2, title: "Coding" },
-  //   { id: 3, title: "Do tasks" },
-  // ]);
+    // const [todoList, setTodoList] = useState([
+    //   { id: 1, title: "Toothbrush" },
+    //   { id: 2, title: "Coding" },
+    //   { id: 3, title: "Do tasks" },
+    // ]);
 
-  // const [postList, setPostList] = useState();
-  // const [pagination, setPagination] = useState({
-  //   _page: 1,
-  //   _limit: 10,
-  //   _totalRows: 11,
-  // });
+    // const [postList, setPostList] = useState();
+    // const [pagination, setPagination] = useState({
+    //   _page: 1,
+    //   _limit: 10,
+    //   _totalRows: 11,
+    // });
 
-  // const [filters, setFilters] = useState({
-  //   _limit: 10,
-  //   _page: 1,
-  //   title_like: ''
-  // });
+    // const [filters, setFilters] = useState({
+    //   _limit: 10,
+    //   _page: 1,
+    //   title_like: ''
+    // });
 
-  // useEffect(() => {
-  //   const fetchPostList = async () => {
-  //     try {
-  //       const paramsString = queryString.stringify(filters);
-  //       const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
-  //       const response = await fetch(requestUrl);
-  //       const responseJson = await response.json();
+    // useEffect(() => {
+    //   const fetchPostList = async () => {
+    //     try {
+    //       const paramsString = queryString.stringify(filters);
+    //       const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramsString}`;
+    //       const response = await fetch(requestUrl);
+    //       const responseJson = await response.json();
 
-  //       const { data, pagination } = responseJson;
+    //       const { data, pagination } = responseJson;
 
-  //       setPostList(data);
-  //       setPagination(pagination);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+    //       setPostList(data);
+    //       setPagination(pagination);
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   };
 
-  //   fetchPostList();
-  // }, [filters]);
+    //   fetchPostList();
+    // }, [filters]);
 
-  // const handlePageChange = (newPage) => {
-  //   console.log(newPage);
-  //   setFilters({
-  //     ...filters,
-  //     _page: newPage,
-  //   });
-  // };
+    // const handlePageChange = (newPage) => {
+    //   console.log(newPage);
+    //   setFilters({
+    //     ...filters,
+    //     _page: newPage,
+    //   });
+    // };
 
-  // const handleTodoClick = (todo) => {
-  //   const index = todoList.findIndex((x) => x.id === todo.id);
-  //   if (index < 0) return;
+    // const handleTodoClick = (todo) => {
+    //   const index = todoList.findIndex((x) => x.id === todo.id);
+    //   if (index < 0) return;
 
-  //   const newTodoList = [...todoList];
-  //   newTodoList.splice(index, 1);
-  //   setTodoList(newTodoList);
-  // };
+    //   const newTodoList = [...todoList];
+    //   newTodoList.splice(index, 1);
+    //   setTodoList(newTodoList);
+    // };
 
-  // const handleTodoFormSubmit = (formValues) => {
-  //   const newTodo = {
-  //     id: todoList[todoList.length - 1].id + 1,
-  //     ...formValues,
-  //   };
+    // const handleTodoFormSubmit = (formValues) => {
+    //   const newTodo = {
+    //     id: todoList[todoList.length - 1].id + 1,
+    //     ...formValues,
+    //   };
 
-  //   console.log(newTodo);
-  //   const newTodoList = [...todoList];
-  //   newTodoList.push(newTodo);
-  //   setTodoList(newTodoList);
-  // };
+    //   console.log(newTodo);
+    //   const newTodoList = [...todoList];
+    //   newTodoList.push(newTodo);
+    //   setTodoList(newTodoList);
+    // };
 
-  // const handleFilterChange = (newFilter) =>{
-  //   console.log("new filters", newFilter);
-  //   setFilters({
-  //     ...filters,
-  //     _page: 1,
-  //     title_like: newFilter.searchTerm,
-  //   })
-  // }
+    // const handleFilterChange = (newFilter) =>{
+    //   console.log("new filters", newFilter);
+    //   setFilters({
+    //     ...filters,
+    //     _page: 1,
+    //     title_like: newFilter.searchTerm,
+    //   })
+    // }
 
-  // const [showClock, setShowClock] = useState(true);
+    // const [showClock, setShowClock] = useState(true);
 
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="app">
-      <h1>React hooks - Clock</h1>
+    return (
+        <div className="app">
+            <h1>React hooks - Clock</h1>
 
-      <p>{count}</p>
+            <p>{count}</p>
 
-      <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
 
-      <Hero name="Van Then" />
+            <Hero name="Van Then" />
 
-      {/* <MagicBox /> */}
+            {/* <MagicBox /> */}
 
-      {/* {showClock && <Clock />}
+            {/* {showClock && <Clock />}
       <BetterClock /> */}
 
-      {/* <button onClick={() => setShowClock(false)}>Hide clock</button> */}
-      {/* <PostFilterForm onSubmit={handleFilterChange}/>
+            {/* <button onClick={() => setShowClock(false)}>Hide clock</button> */}
+            {/* <PostFilterForm onSubmit={handleFilterChange}/>
       <PostList posts={postList} />
       <Pagination pagination={pagination} onPageChange={handlePageChange} /> */}
 
-      {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
-      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
-    </div>
-  );
+            {/* <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+            {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+        </div>
+    );
+    // I changed here
 }
 
 export default App;
---
+++ react-hooks-basic/src/idk.js
@@ -0,0 +1,1 @@
+// I don't know why i create this file
